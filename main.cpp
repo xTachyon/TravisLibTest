710334e84067a24e7f783269cfa6a860206938db
Damian Andrei <xTachyon@users.noreply.github.com>
2016-11-14 14:38:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 //#include <boost/endian/conversions.hpp>
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
 
 int main(int argc, char** argv)
 {
-
+  if (argc != 2)
+  {
+    std::cerr << "Usage: <folder>";
+    return 1;
+  }
+  
+  fs::path path(argv[1]);
+  if (!fs::exists(path) || !fs::is_directory(path))
+  {
+    std::wcerr << path.wstring() + L" does not exists or isn't a directory.";
+    return 2;
+  }
 
   return 0;
 }
