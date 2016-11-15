3327f3ff2d68a9db87aade8cc18ffa83b6c3bd65
Damian Andrei <xTachyon@users.noreply.github.com>
2016-11-15 15:32:37 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
-//#include <boost/endian/conversions.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/endian/conversion.hpp>
 
 namespace fs = boost::filesystem;
 
 int main(int argc, char** argv)
 {
-  if (argc != 2)
+  fs::path path;
+  if (argc == 2) path = argv[1];
+  else
   {
-    std::cerr << "Usage: <folder>";
-    return 1;
+    std::wstring str;
+    std::getline(std::wcin, str);
+    path = str;
   }
-  
-  fs::path path(argv[1]);
+
   if (!fs::exists(path) || !fs::is_directory(path))
   {
     std::wcerr << path.wstring() + L" does not exists or isn't a directory.";
     return 2;
   }
+
+  for (const auto& index : fs::recursive_directory_iterator(path))
+  {
+    std::wcout << index.path().wstring() << L'\n';
+  }
+
+  int x = 5;
+  for (int i = 0; i < sizeof(int); ++i)
+    std::cout << static_cast<int>(reinterpret_cast<const std::uint8_t*>(&x)[i]) << ' ';
+  std::cout << '\n';
+
+  boost::endian::native_to_big_inplace(x);
+  for (int i = 0; i < sizeof(int); ++i)
+    std::cout << static_cast<int>(reinterpret_cast<const std::uint8_t*>(&x)[i]) << ' ';
 
   return 0;
 }
