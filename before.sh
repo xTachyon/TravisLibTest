cfbd5936d3f18e82f5dc6aa3b2b253b67da64c33
Damian Andrei <xTachyon@users.noreply.github.com>
2016-11-20 17:13:59 +0200

    Delete before.sh

--- before.sh
+++
@@ -1,8 +0,0 @@
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install gcc-6 g++-6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
-
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:george-edison55/cmake-3.x
-sudo apt-get update
