3a324324b5464b38b4285fb2688da31bd3751c22
Damian Andrei <xTachyon@users.noreply.github.com>
2016-12-07 22:32:34 +0200

    Create travisbuild.sh

---
+++ travisbuild.sh
@@ -0,0 +1,15 @@
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 100 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+g++ -v
+cmake --version
+
+if [ -d "/boost_1_62_0/bin.v2/" ];
+then
+
+else
+  wget http://kent.dl.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.bz2
+  tar -xjf boost_1_62_0.tar.bz2
+  cd boost_1_62_0/
+  sudo ./bootstrap.sh
+  sudo ./b2 install -j 8
+  cd ..
+fi
