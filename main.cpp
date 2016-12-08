64f5a73a42a4fe71bdb165189d6042d6b1e67964
Damian Andrei <xTachyon@users.noreply.github.com>
2016-12-08 13:05:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
-int main() {}
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/endian/conversions.hpp>
+
+namespace fs = boost::filesystem;
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Usage: <folder>";
+    return 1;
+  }
+  
+  fs::path path(argv[1]);
+  if (!fs::exists(path) || !fs::is_directory(path))
+  {
+    std::wcerr << path.wstring() + L" does not exists or isn't a directory.";
+    return 2;
+  }
+
+  for (const auto& index : fs::recursive_directory_iterator(path))
+    std::wcout << index.path().wstring() << L'\n';
+
+  return 0;
+}
