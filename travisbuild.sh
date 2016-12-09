249dc2ba7332c8fe482ae954570c749f21372c43
Damian Andrei <xTachyon@users.noreply.github.com>
2016-12-09 15:45:07 +0200

    Update travisbuild.sh

--- travisbuild.sh
+++ travisbuild.sh
@@ -1,7 +1,7 @@
 set -x
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 100 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
-if [ ! -f "boost_1_62_0/b2" ]; then
+if [ ! -d "libs/boost/lib/" ]; then
   wget http://kent.dl.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.bz2
   tar -xjf boost_1_62_0.tar.bz2
   cd boost_1_62_0/
@@ -10,10 +10,10 @@ if [ ! -f "boost_1_62_0/b2" ]; then
   cd ..
 fi
 
-cd boost_1_62_0/
-sudo ./bootstrap.sh --prefix=../libs/boost/
-sudo ./b2 install -j 8
-cd ..
+#cd boost_1_62_0/
+#sudo ./bootstrap.sh --prefix=../libs/boost/
+#sudo ./b2 install -j 8
+#cd ..
 
 ls boost_1_62_0/
 ls -l libs/boost/
